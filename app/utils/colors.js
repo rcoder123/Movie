a36380fb3ece15d9d17a4f9758fda10c77e82cef
“RehanR” <rehanr0907@gmail.com>
2020-09-07 21:52:50 +0530

    Initial commit

---
+++ app/utils/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+    border: 'rgba(0, 0, 0, 0.1)',
+    backgroundTransparent: 'rgba(0, 0, 0, 0.04)',
+    black: 'rgb(0,0,0)',
+};
---
+++ ios/Pods/Target Support Files/Pods-MMRNInterview-MMRNInterviewTests/Pods-MMRNInterview-MMRNInterviewTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MMRNInterview_MMRNInterviewTests : NSObject
+@end
+@implementation PodsDummy_Pods_MMRNInterview_MMRNInterviewTests
+@end
