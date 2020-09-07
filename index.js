035b0f102bddadfeda9b3ed76b5b5d501e870aa0
“RehanR” <rehanr0907@gmail.com>
2020-09-07 22:02:27 +0530

    Added a config files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import Navigation from '@navigation';
-import {name as appName} from './app.json';
+import 'react-native-gesture-handler'
+import { AppRegistry } from 'react-native'
+import Navigation from '@navigation'
+import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => Navigation);
+AppRegistry.registerComponent(appName, () => Navigation)
